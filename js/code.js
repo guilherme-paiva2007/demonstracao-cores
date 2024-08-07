d74e9e7db2309339c79ba2c1e992dc787808ab67
guilherme-paiva2007 <guilhermepaiva2007h@gmail.com>
2024-08-07 17:50:48 -0300

    Muita coisa dnv

--- js/code.js
+++ js/code.js
@@ -51,6 +51,11 @@ function writeInside(element, text) {
     }
 };
 
+/**
+ * Transforma um código de cor em um array com os códigos de cor
+ * @param {Color.codeText} text 
+ * @returns {Color.codes}
+ */
 function getColorTextToNumbers(text) {
     let colorType = "";
     let numbers = [];
@@ -68,13 +73,18 @@ function getColorTextToNumbers(text) {
         });
     }
     if (colorType == "hex") {
-        numbers[0] = text.slice(0, 2);
-        numbers[1] = text.slice(2, 4);
-        numbers[2] = text.slice(4, 6);
+        numbers[0] = parseInt(text.slice(0, 2), 16);
+        numbers[1] = parseInt(text.slice(2, 4), 16);
+        numbers[2] = parseInt(text.slice(4, 6), 16);
     }
     return numbers;
 };
 
+/**
+ * Coleta as cores de fundo e index das classes de uma folha de estilo.
+ * @param {number} styleSheet Número da folha de estilo
+ * @returns {[HTMLClass.Name, Color.codes, HTMLClass.Index]}
+ */
 function getCSSBGColorRules(styleSheet) {
     let listLength = 0;
     let classRules = [];
@@ -99,10 +109,29 @@ function getCSSBGColorRules(styleSheet) {
     return list;
 };
 
-function isDark(colorArr = [0, 0, 0]) { // FUNÇÃO RETIRADA E ADAPTADA DE: https://www.w3schools.com/lib/w3color.js (LINHA 82) | UTILIZADA NA PÁGINA: https://www.w3schools.com/css/css_colors_rgb.asp
-    return (((colorArr[0] * 299 + colorArr[1] * 587 + colorArr[2] * 114) / 1000) < 150);
+/**
+ * Verifica se a cor é escura ou clara.
+ * @param {Color.codes} colorArr 
+ * @param {boolean} invert Determina se inverterá o retorno
+ * @returns {boolean}
+ */
+function isDark(colorArr = [0, 0, 0], invert) { // FUNÇÃO RETIRADA E ADAPTADA DE: https://www.w3schools.com/lib/w3color.js (LINHA 82) | UTILIZADA NA PÁGINA: https://www.w3schools.com/css/css_colors_rgb.asp
+    let value = (((colorArr[0] * 299 + colorArr[1] * 587 + colorArr[2] * 114) / 1000) < 150);
+    if (invert) value = !value;
+    return value;
 }
 
+
+// ESCREVER UMA FUNÇÃO PARA CONVERTER OS TIPOS DE CORES, OU CONJUNTO DE FUNÇÕES QUE CONVERTE OUTROS TIPOS NO DESEJADO
+
+
+/**
+ * 
+ * @param {string} colorClass 
+ * @param {Color.codes} colors Aceita apenas arrays de código (ou codeText de hexadecimal).
+ * @param {number} styleSheet 
+ * @param {"rgb" | "rgba" | "hsl" | "hsla" | "hex" | "hexa"} colorType 
+ */
 function changeColor(colorClass, colors = [0, 0, 0, 1], styleSheet, colorType) {
     let documentRules = document.styleSheets[styleSheet].cssRules;
     let colorRules = [];
@@ -121,7 +150,7 @@ function changeColor(colorClass, colors = [0, 0, 0, 1], styleSheet, colorType) {
             colorText = `hsl(${colors[0]}, ${colors[1]}%, ${colors[2]}%)`;
             break;
         case "hsla":
-            colorText = `hsla(${colors[0]}%, ${colors[1]}%, ${colors[2]}, ${colors[3]})`;
+            colorText = `hsla(${colors[0]}, ${colors[1]}%, ${colors[2]}%, ${colors[3]})`;
             break;
         case "hex":
             if (typeof colors == "string") { colorText = `#${colors.removeText('#')}` }
@@ -139,6 +168,21 @@ function changeColor(colorClass, colors = [0, 0, 0, 1], styleSheet, colorType) {
     colorRules.forEach((colorRuleIndex) => {
         document.styleSheets[styleSheet].cssRules[colorRuleIndex].style.backgroundColor = colorText;
     })
+}
+
+/**
+ * Junta códigos isolados de cores em um texto de `255, 255, 255`
+ * @param {[ColorCode_1, ColorCode_2, ColorCode_3, ColorCode_Alpha]} colorCodes 
+ */
+function concatColor(colorCodes = [0, 0, 0], alphaBreak = true) {
+    let text = "";
+    if (colorCodes.length == 3) alphaBreak = false;
+    if (alphaBreak) {
+        text = colorCodes.slice(0, -1).join(', ').concat('<br>' + colorCodes[3])
+    } else {
+        text = colorCodes.join(', ')
+    }
+    return text;
 }
 
 class Color {
@@ -256,6 +300,20 @@ class Color {
     }
 }
 
+function update(inputs, targetClass, type) { // Posta as cores na classe-css
+    // Também atualiza a legenda || CUIDADO COM HEXADECIMAL, UTILIZAR CONSTRUTOR DE COR (codeText)
+    // Verificar os inputs, ativando as dinamicas que guardam.
+    let colors = [];
+    Object.values(inputs).forEach((input, index) => {
+        if (document.getElementById(input.id) !== null) input.value = document.getElementById(input.id).value
+        colors[index] = input.value
+        if (input.dyname !== undefined) input.dyname()
+    })
+    colors[3] = colors[3] / 100
+
+    changeColor(targetClass, colors, ColorsStyleSheet, type)
+}
+
 class Slide {
     /**
      * Cria um objeto de slide com funcionalidades internas, podendo ser integrado à qualquer classe de cor com seus respectivos slides e legendas.
@@ -268,10 +326,12 @@ class Slide {
      * @param {{input1: String.ID, input2: String.ID, input3: String.ID, alpha: String.ID}} colorSlidesIDs Objeto de configuração dos três slides utilizados e, se necessário, o uso de um quarto para Alpha.
      * @param {String.ID} descID Elemento que atualizará constantemente com o valor do texto. Não é obrigatório, deixe vazio ou indefinido para anulá-lo.
      */
-    constructor(colorType = "rgb", targetClass = "", colorInputsIDs = {input1: "", input2: "", input3: "", alpha: ""}, descID = "") {
+    constructor(colorType, targetClass = "", colorInputsIDs = {input1: "", input2: "", input3: "", alpha: ""}, descID = "") {
         this.type = colorType;
         this.targetClass = targetClass;
         this.descID = descID;
+        this.usesAlpha = false;
+        if (this.type == "hexa" || this.type == "rgba" || this.type == "hsla") this.usesAlpha = true;
         if (!colorType.isIn(['rgb', 'rgba', 'hsl', 'hsla', 'hex', 'hexa'])) this.colorType = "rgb";
         Object.values(colorInputsIDs).forEach(id => {
             if (document.getElementById(id) == null || document.getElementById(id).type !== "range") return console.log("Só são permitidos inputs do tipo \"range\"");
@@ -293,7 +353,7 @@ class Slide {
             /**
              * 
              * @param {String.ID} id 
-             * @param {"rgb/hex" | "hue" | "sat/lig" | "alpha"} type
+             * @param {"rgb/hex" | "hue" | "sat" | "lig" | "alpha"} type
              */
             constructor(id, type) {
                 this.id = id;
@@ -301,21 +361,24 @@ class Slide {
                 this.dyname = undefined;
                 this.type = type;
 
-                if (this.type == "rgb/hex") { this.min = "0"; this.max = "255" }
-                if (this.type == "hue") { this.min = "0"; this.max = "360" }
-                if (this.type == "sat/lig") { this.min = "0"; this.max = "100" }
-                if (this.type == "alpha") { this.min = "0"; this.max = "255" }
+                if (this.type == "rgb/hex") { this.min = "0"; this.max = "255"; this.value = 128; }
+                if (this.type == "hue") { this.min = "0"; this.max = "360"; this.value = 0; }
+                if (this.type == "sat") { this.min = "0"; this.max = "100"; this.value = 100; }
+                if (this.type == "lig") { this.min = "0"; this.max = "100"; this.value = 50; }
+                if (this.type == "alpha") { this.min = "0"; this.max = "100"; this.value = 100; }
 
                 document.getElementById(this.id).min = this.min
                 document.getElementById(this.id).max = this.max
+
+                document.getElementById(id).value = this.value;
             }
         }
 
         let inputTypes = [];
         if (this.type == "rgb") inputTypes = ["rgb/hex", "rgb/hex", "rgb/hex"]
         if (this.type == "rgba") inputTypes = ["rgb/hex", "rgb/hex", "rgb/hex", "alpha"]
-        if (this.type == "hsl") inputTypes = ["hue", "sat/lig", "sat/lig"]
-        if (this.type == "hsla") inputTypes = ["hue", "sat/lig", "sat/lig", "alpha"]
+        if (this.type == "hsl") inputTypes = ["hue", "sat", "lig"]
+        if (this.type == "hsla") inputTypes = ["hue", "sat", "lig", "alpha"]
         if (this.type == "hex") inputTypes = ["rgb/hex", "rgb/hex", "rgb/hex"]
         if (this.type == "hexa") inputTypes = ["rgb/hex", "rgb/hex", "rgb/hex", "alpha"]
 
@@ -323,20 +386,10 @@ class Slide {
         this.inputs["1"] = new input(colorInputsIDs.input1, inputTypes[0])
         this.inputs["2"] = new input(colorInputsIDs.input2, inputTypes[1])
         this.inputs["3"] = new input(colorInputsIDs.input3, inputTypes[2])
-        if (this.type == "hexa" || this.type == "rgba" || this.type == "hsla") this.inputs.alpha = new input(colorInputsIDs.alpha, inputTypes[3])
+        if (this.usesAlpha) this.inputs["alpha"] = new input(colorInputsIDs.alpha, inputTypes[3])
 
-        this.update = function update(inputs = this.inputs, targetClass = this.targetClass, type = this.type) { // Posta as cores na classe-css
-            // Também atualiza a legenda || CUIDADO COM HEXADECIMAL, UTILIZAR CONSTRUTOR DE COR (codeText)
-            // Verificar os inputs, ativando as dinamicas que guardam.
-            let colors = [];
-            Object.values(inputs).forEach((input, index) => {
-                if (document.getElementById(input.id) !== null) input.value = document.getElementById(input.id).value
-                colors[index] = input.value
-                if (input.dyname !== undefined) input.dyname()
-                console.log(colors, input)
-            })
-
-            changeColor(targetClass, colors, ColorsStyleSheet, type)
+        this.update = function internUpdate() {
+            update(this.inputs, this.targetClass, this.type)
         }
 
         /**
@@ -359,38 +412,69 @@ class Slide {
          * @param {input} input 
          * @param {string} color codeText da cor. Recomendado utilizar construtor de cor.
          */
-        this.dyname = function dyname(dynamic, input, color) {
+        this.dyname = function dyname(dynamic, input, color, auxConfig) {
             if(input == undefined) return console.log('Utilize um input válido');
             if(dynamic == undefined) return console.log('Utilize uma dinâmica válida');
 
             let dynamicUpdate = function () {
-                dynamic(input.id, color);
+                dynamic(input.id, color, input, auxConfig);
             }
 
             input.dyname = dynamicUpdate
+            this.update()
         }
 
         this.update();
-        update = this.update
-        Object.values(this.inputs).forEach((input) => {
-            document.getElementById(input.id).oninput = update;
-        })
+        
+        document.getElementById(this.inputs[1].id).oninput = () => { update(this.inputs, this.targetClass, this.type) }
+        document.getElementById(this.inputs[2].id).oninput = () => { update(this.inputs, this.targetClass, this.type) }
+        document.getElementById(this.inputs[3].id).oninput = () => { update(this.inputs, this.targetClass, this.type) }
+        if (this.usesAlpha) document.getElementById(this.inputs["alpha"].id).oninput = () => { update(this.inputs, this.targetClass, this.type) }
     }
 }
 
 let dynamicInputs = { // Funções que dinamizam os inputs através da funcionalidadea onfocus HTML
-    hue: function hueAutoSlide(slideID) {
+    hue: function hueAutoSlide(slideID, color, input, auxConfig) {
         let slideElement = document.getElementById(slideID)
         if (slideElement.min !== 0) slideElement.min = 0;
         if (slideElement.max !== 360) slideElement.max = 360;
+
+        let auxValues = {}
+        auxValues.lig = document.getElementById(auxConfig.ligID).value;
+        auxValues.sat = document.getElementById(auxConfig.satID).value;
     
-        let newColor = new Color('hsla', [parseInt(slideElement.value), 100, 50, 0.7]).codeText
+        let newColor = new Color('hsla', [parseInt(slideElement.value), parseInt(auxValues.sat), parseInt(auxValues.lig), 1.0]).codeText
         document.getElementById(slideID).style.backgroundColor = newColor;
+        document.getElementById(slideID).style.transition = "0s";
     },
-    fading: function fadingSlide(slideID) {
-        // Aumenta a intensidade conforme aumenta o valor do input
+    /**
+     * Transiciona entre duas cores de escolha.
+     * @param {HTMLClass.ID} slideID 
+     * @param {{color1: Color.codes, color2: Color.codes, colorsType: "rgb" | "hex" | "hsl"}} auxConfig Utilize cores de mesmo formato, sem alpha e em formato de códigos para não causar erros.
+     */
+    fading: function fadingSlide(slideID, color, input, auxConfig) { // ATUALIZAR FUTURAMENTE PARA COMPORTAR MAIS CORES E DE DIFERENTES TIPOS
+        let avarageColor = [];
+        let slide = {
+            pos: document.getElementById(slideID).value - document.getElementById(slideID).min,
+            end: document.getElementById(slideID).max - document.getElementById(slideID).min,
+        }
+        let gap_end = slide.end - slide.pos;
+        let gap_start = slide.pos;
+
+        avarageColor[0] = (auxConfig.color1[0] * gap_end + auxConfig.color2[0] * gap_start) / slide.end;
+        avarageColor[1] = (auxConfig.color1[1] * gap_end + auxConfig.color2[1] * gap_start) / slide.end;
+        avarageColor[2] = (auxConfig.color1[2] * gap_end + auxConfig.color2[2] * gap_start) / slide.end;
+
+        let newColor = new Color(auxConfig.colorsType, avarageColor).codeText;
+        document.getElementById(slideID).style.backgroundColor = newColor;
+        document.getElementById(slideID).style.transition = "0s";
     },
-    fill: function fillSlide(slideID, color) { // Atualizar todos para inputOBJ (objetos de input[range] do construtor de Slide)
+    /**
+     * Preenche o input range de acordo com seu valor.
+     * @param {HTMLClass.ID} slideID 
+     * @param {Color.codeText} color 
+     */
+    fill: function fillSlide(slideID, color) {
         let slideElement = document.getElementById(slideID);
         let min = slideElement.min;
         let max = slideElement.max - min;
@@ -399,8 +483,15 @@ let dynamicInputs = { // Funções que dinamizam os inputs através da funcional
         let range = Math.floor(value / max * 100);
         newBackground = `linear-gradient(90deg, ${color} ${range}%, rgba(127, 127, 127, 0.5) ${range+1}%)`; // Adicionar dinâmica com construtores de input para implementar hover adaptado em HTML (onmouseenter e onmouseleave)
         document.getElementById(slideID).style.background = newBackground
+    },
+    /**
+     * Permite a alteração do alpha do input de acordo com quão preenchido está.
+     * @param {String.ID} slideID ID do input
+     * @param {Color.codeText} color 
+     * @param {input} input
+     * @param {{colorType: "rgb" | "hsl" | "hex"}} auxConfig
+     */
+    alpha: function alpha(slideID, color, input, auxConfig) {
+        
     }
 }
-
-
-const colorSelect2 = new Slide("rgb", "colorSelect2", {input1: "colorSelect2HUE", input2: "colorSelect2SATUR", input3: "colorSelect2LIGHT", alpha: "colorSelect2ALPHA"})
