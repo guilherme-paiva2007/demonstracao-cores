6c8a424bf30fc31c7da5145f6987825ea3916275
guilherme-paiva2007 <guilhermepaiva2007h@gmail.com>
2024-08-13 23:55:28 -0300

    finish color class

--- js/main.js
+++ js/main.js
@@ -90,6 +90,7 @@ String.prototype.reverse = function reverse() {
 const colorCorrect = {
     rgb: function correctRGB(codes, alpha) {
         codes = codes.map((code, index) => {
+            if (typeof code !== "number") code = 0;
             if(index == 3 && alpha) {
                 if (code > 1) code = 1;
                 if (code < 0) code = 0;        
@@ -102,6 +103,7 @@ const colorCorrect = {
     },
     hsl: function correctHSL(codes, alpha) {
         codes = codes.map((code, index) => {
+            if (typeof code !== "number") code = 0;
             if(index == 3 && alpha) {
                 if (code > 1) code = 1;
                 if (code < 0) code = 0;        
@@ -118,6 +120,14 @@ const colorCorrect = {
         return codes
     },
     hex: function correctHEX(codes, alpha) {
+        const hexaValues = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'a', 'b', 'c', 'd', 'e', 'f']
+        codes = codes.map(code => {
+            if (typeof code !== "string") { code = "00" }
+            code.split('').forEach(char => {
+                if (!char.isIn(hexaValues)) code = "00"
+            })
+            return code;
+        })
         codes = codes.map(code => parseInt(code, 16))
         codes = codes.map((code, index) => {
             if (code > 255) code = 255;
@@ -125,12 +135,72 @@ const colorCorrect = {
             return code;
         })
         codes = codes.map(code => code.toString(16))
+        codes = codes.map(code => {
+            if (code.length < 2) code = '0' + code;
+            return code;
+        })
         return codes
+    },
+    codesLength: function correctCodesLength(codes, alpha) {
+        let amount = 3;
+        if (alpha) amount = 4;
+
+        if (codes.length > amount) codes = codes.slice(0, amount);
+        if (codes.length < amount) {
+            codes = codes.fillUntil(0, 3);
+            if (alpha) codes[3] = 1;
+        }
+        return codes;
     }
 }
 
 class Color {
+    /**
+     * Cria um objeto de cor com seus códigos, fornecendo textos de função e opções para serem alteradas.
+     * @param {"rgb" | "rgba" | "hsl" | "hsla" | "hex" | "hexa"} type 
+     * @param {[string|number, string|number, string|number]} codes 
+     */
     constructor(type, codes) {
-        // hex só com hexadecimal agora
+        this.type = type;
+        if (typeof this.type !== "string") this.type = "null";
+        if (!this.type.isIn(["rgb", "rgba", "hsl", "hsla", "hex", "hexa"])) this.type = "null";
+
+        this.alpha = false;
+        if (this.type.isIn(["rgba", "hsla", "hexa"])) this.alpha = true;
+
+        this.codes = codes;
+        this.codes = colorCorrect.codesLength(this.codes, this.alpha);
+        switch (this.type) {
+            case "rgb":
+            case "rgba":
+                this.codes = colorCorrect.rgb(this.codes, this.alpha);
+                break;
+            case "hsl":
+            case "hsla":
+                this.codes = colorCorrect.hsl(this.codes, this.alpha);
+                break;
+            case "hex":
+            case "hexa":
+                this.codes = colorCorrect.hex(this.codes, this.alpha);
+                break;
+            case "null":
+                this.codes = [0, 0, 0, 1];
+                this.type = "rgb";
+                break;
+        }
+        
+        this.codeText = "";
+        switch (this.type) {
+            case "rgb":
+            case "rgba":
+            case "hsl":
+            case "hsla":
+                this.codeText = `${this.type}(${this.codes.join(', ')})`
+                break;
+            case "hex":
+            case "hexa":
+                this.codeText = `#${this.codes.join('')}`
+                break;
+        }
     }
 }
